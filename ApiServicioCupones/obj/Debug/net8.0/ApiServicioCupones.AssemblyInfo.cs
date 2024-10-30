6443d533774d83ffd089b417ff438950f3201d76
Acosta <cristian.acosta01@icloud.com>
2024-10-30 18:29:23 -0300

    cambio menor, git test

--- ApiServicioCupones/obj/Debug/net8.0/ApiServicioCupones.AssemblyInfo.cs
+++ ApiServicioCupones/obj/Debug/net8.0/ApiServicioCupones.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("62630b71-7e5b-44ef-aeaa-44b960a45957")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiServicioCupones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e34b5455cd8ed9d9950857b2d468c974a0b12cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c510632f4b4c9fec88dc87cbdf10c07845bfb514")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiServicioCupones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiServicioCupones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
